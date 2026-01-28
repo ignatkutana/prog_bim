f377ba6b1e6a85e18177e732048c9714aec0d19e
ignatkutana <ignatkutana@gmail.com>
2026-01-28 15:31:44 +0300

    Add files via upload

---
+++ лабораторная работа 2/task1.py
@@ -0,0 +1,18 @@
+money_capital = 20000  # Подушка безопасности
+salary = 5000  # Ежемесячная зарплата
+spend = 6000  # Траты за первый месяц
+increase = 0.05  # Ежемесячный рост цен
+
+# TODO Посчитайте количество  месяцев, которое можно протянуть без долгов
+months = 0
+while True:
+    available_money = money_capital + salary
+    if available_money >= spend:
+        money_capital = available_money - spend
+        months += 1
+        spend *= (1 + increase)
+    else:
+        break
+
+print("Количество месяцев, которое можно протянуть без долгов:", months)
+
